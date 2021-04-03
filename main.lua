b8ccd0f9b3eeea30019b06476a21a90496f522cc
Thomas <contact@thomas-george.com>
2021-04-03 13:52:35 +0200

    project skeleton

---
+++ main.lua
@@ -0,0 +1,17 @@
+-- display logs in console during execution
+io.stdout:setvbuf('no')
+
+-- Pixel Art mode
+love.graphics.setDefaultFilter("nearest")
+
+
+function love.load()
+end
+
+
+function love.update(dt)
+end
+
+
+function love.draw()
+end
